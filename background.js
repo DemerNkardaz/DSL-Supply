5f1ea9376c606afd9a08f5144751ba60dbe3713c
DemerNkardaz <92405785+DemerNkardaz@users.noreply.github.com>
2025-03-03 08:31:26 +0200

    А

---
+++ background.js
@@ -0,0 +1,7 @@
+console.log("DSL Supply Background Script активирован.");
+
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  console.log("Сообщение от content.js: ", message);
+  sendResponse({ status: "ok" });
+});
---
+++ content.js
@@ -0,0 +1,8 @@
+// content.js — Этот файл будет взаимодействовать со страницей
+
+console.log("DSL Supply активирован");
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log("Контент-скрипт был успешно загружен на страницу.");
+  
+});
